0076a087c7e405cc446d081861c5ca0543b9af74
MoiisesDaviid <mdpolo1995@gmail.com>
2019-07-23 23:51:06 -0500

    varios

--- views/include/header.php
+++ views/include/header.php
@@ -54,14 +54,14 @@
     <div class="row">
       <div class="col-xs-6">
         <div class="th-text pull-left">
-          <div class="th-item"> <a href="#"><i class="fa fa-phone"></i> 05-460789986</a> </div>
-          <div class="th-item"> <a href="#"><i class="fa fa-envelope"></i> MAIL@STARHOTEL.COM </a></div>
+          <div class="th-item"> <a href="#"><i class="fa fa-phone"></i> 578 1068 EXT: 127</a> </div>
+          <div class="th-item"> <a href="#"><i class="fa fa-envelope"></i> referenciaclinicasinaisvitais@gmail.com</a></div>
         </div>
       </div>
       <div class="col-xs-6">
         <div class="th-text pull-right">
           <div class="th-item">
-            <div class="btn-group">
+            <!-- <div class="btn-group">
               <button class="btn btn-default btn-xs dropdown-toggle js-activated" type="button" data-toggle="dropdown"> English <span class="caret"></span> </button>
               <ul class="dropdown-menu">
                 <li> <a href="#">ENGLISH</a> </li>
@@ -69,10 +69,10 @@
                 <li> <a href="#">GERMAN</a> </li>
                 <li> <a href="#">SPANISH</a> </li>
               </ul>
-            </div>
+            </div> -->
           </div>
           <div class="th-item">
-            <div class="social-icons"> <a href="#"><i class="fa fa-facebook"></i></a> <a href="./src/#"><i class="fa fa-twitter"></i></a> <a href="./src/#"><i class="fa fa-youtube-play"></i></a> </div>
+            <div class="social-icons"> <a href="https://www.facebook.com/Cl%C3%ADnica-Sinais-Vitais-433606847245211/?modal=admin_todo_tour" target="_blank"><i class="fa fa-facebook"></i></a> <a href="./src/#"><i class="fa fa-twitter"></i></a> <a href="./src/#"><i class="fa fa-youtube-play"></i></a> </div>
           </div>
         </div>
       </div>
